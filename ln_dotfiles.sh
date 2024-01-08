b866d57a5dbecff4b4156c6832b406dd0fab4174
natr <yc5@tju.edu.cn>
2024-01-09 00:08:18 +0800

    add .condarc and .X/x file

---
+++ ln_dotfiles.sh
@@ -0,0 +1,8 @@
+ln -s $HOME/repos/dotfiles/.condarc $HOME
+ln -s $HOME/repos/dotfiles/.Xauthority $HOME
+ln -s $HOME/repos/dotfiles/.Xdefaults $HOME
+ln -s $HOME/repos/dotfiles/.Xmodmap $HOME
+ln -s $HOME/repos/dotfiles/.Xresources $HOME
+
+# TODO: Use 'for' to simplify code.
+#.Xauthority .Xdefaults .Xmodmap .Xresources
--- readme.md
+++ readme.md
@@ -5,3 +5,4 @@ clone oh-my-zsh repos before using .zshrc
 
 # To-do
 Create a scrips to backup system (package, file)
+make origin the default remote.
